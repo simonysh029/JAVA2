9d056dd7f53339635df8d35c95e6f77a51b4f9ef
simonysh029 <simonysh029@naver.com>
2023-05-02 00:07:29 +0900

    first commit

---
+++ chap02/src/sec02/exam05/StringExample.java
@@ -0,0 +1,15 @@
+package sec02.exam05;
+
+public class StringExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// String은 HTML의 Class와 같은 개념, 그래서 String을 대문자로 쓴다.
+		// 이미 프로그램 속에 의미가 내재되어있는 것이다.
+		String name = "홍길동";
+		String job = "프로그래머";
+		System.out.println(name);
+		System.out.println(job);
+	}
+
+}
---
+++ chap02/src/sec02/exam06/EscapeExample.java
@@ -0,0 +1,17 @@
+package sec02.exam06;
+
+public class EscapeExample {
+
+	public static void main(String[] args) {
+		//  \가 escape문자열
+		//	print와 println 차이 알기
+		//	\t 는 탭 한번
+		//	\n 은 줄 바꿈
+		System.out.println("번호\t이름\t직업");
+		System.out.print("행 단위 출력\n");
+		System.out.print("행 단위 출력\n");
+		System.out.println("우리는 \"개발자\" 입니다.");
+		System.out.print("봄\\여름\\가을\\겨울");
+	}
+
+}
