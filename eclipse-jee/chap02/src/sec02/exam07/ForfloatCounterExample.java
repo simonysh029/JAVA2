74b36b587f0716b6d6b3fa5c4dd590726414b647
simonysh029 <132255220+simonysh029@users.noreply.github.com>
2023-05-08 03:16:17 +0900

    Delete eclipse-jee directory

--- eclipse-jee/chap02/src/sec02/exam07/ForfloatCounterExample.java
+++
@@ -1,9 +0,0 @@
-package sec02.exam07;
-
-public class ForfloatCounterExample {
-	public static void main(String[] args) {
-		for(float x=0.1f; x<=1.0f; x+=0.1f) {
-			System.out.println(x);
-		}
-	}
-}
