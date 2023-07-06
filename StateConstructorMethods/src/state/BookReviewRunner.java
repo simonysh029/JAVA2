6fa476d15684bfaca068c169add84ffa06b0456f
simonysh029 <132255220+simonysh029@users.noreply.github.com>
2023-07-06 18:14:31 +0900

    Add files via upload

---
+++ StateConstructorMethods/src/state/BookReviewRunner.java
@@ -0,0 +1,10 @@
+package state;
+
+public class BookReviewRunner {
+	public static void main(String[] args) {
+		Book book = new Book(123, "Object Oriented Programming With Java", "Ranga");
+		book.addReview(new Review(10, "Great Book", 4));
+		book.addReview(new Review(101, "Awesome", 5));
+		System.out.println(book);
+	}
+}
